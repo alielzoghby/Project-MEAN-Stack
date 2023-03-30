b6d84df1becc1d06e9eb13b4ba87f514f43e0490
MontaserHassan <montaser.hassan.bns@gmail.com>
2023-03-30 21:24:47 +0200

    fix bug while pushing

--- back-end/index.js
+++ back-end/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoClient = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
-const routes = require('./routes');
-const { usersController } = require('./controllers/index');
 require('dotenv').config(); // to use file .env
 
 const app = express();
