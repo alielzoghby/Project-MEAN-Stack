f83424b860fdc4de410d03aea53a6dc9ac3a7b67
Ali Saad <el.zo3by@gmail.com>
2023-04-07 06:50:39 +0200

    fix some route

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -5,17 +5,18 @@ import { UserpageComponent } from './userpage/userpage.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { AuthorPageComponent } from './author-page/author-page.component';
 import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './register/signup.component';
 import { BookpageComponent } from './bookpage/bookpage.component';
+
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
   { path: 'profile', component: UserpageComponent },
   { path: 'categories', component: CategoriesComponent },
   { path: 'author', component: AuthorPageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  {path:"bookpage",component: BookpageComponent }
-
+  { path: 'bookpage', component: BookpageComponent },
 ];
 
 @NgModule({
