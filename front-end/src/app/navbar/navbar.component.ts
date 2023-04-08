81226e5c8fb14877636229348e6c2c1c798a7203
Ali Saad <el.zo3by@gmail.com>
2023-04-08 06:35:56 +0200

    fix navbar

--- front-end/src/app/navbar/navbar.component.ts
+++ front-end/src/app/navbar/navbar.component.ts
@@ -25,7 +25,7 @@ export class NavbarComponent implements OnInit {
   }
 
   @HostListener('window:scroll', ['$event']) onscroll() {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 60) {
       this.navbarfixed = true;
     } else {
       this.navbarfixed = false;
