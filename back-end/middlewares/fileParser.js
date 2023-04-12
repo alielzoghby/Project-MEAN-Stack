a4c33705d744a6afb9e03faaf8f1af20c70d33f2
MontaserHassan <montaser.hassan.bns@gmail.com>
2023-04-12 22:59:07 +0200

    add express static handler

--- back-end/middlewares/fileParser.js
+++ back-end/middlewares/fileParser.js
@@ -3,17 +3,18 @@ const { v4: uuidv4 } = require('uuid');
 
 // to store user photo
 const storage = multer.diskStorage({
-  destination: './public/photos/',
+  destination: 'public/photos/',
   filename(req, file, cb) {
     // if(req.file && req.file.filename){
-    console.log(req.file);
+    // console.log(req.file);
     fileUUID = uuidv4();
     cb(null, `${file.originalname}-${fileUUID}`);
     // }
   },
 });
   // upload photo
-const upload = multer({ storage }).single('ProfileImage');
+const upload = multer(
+  { storage: storage }, { limits: { fileSize: 5000000 } }).single('image'); // limit file size to 5 MB)
 
 const fileParser = (req, res, next) => {
   upload(req, res, (err) => {
