e2ca75dccda31f48450174cb112435abf78eb3b8
Ali Saad <el.zo3by@gmail.com>
2023-04-15 17:46:55 +0200

    finsh category section

--- front-end/src/app/userpage/userpage.component.ts
+++ front-end/src/app/userpage/userpage.component.ts
@@ -20,67 +20,59 @@ export class UserpageComponent {
   rating: number = 0;
   Math = Math;
   temp: any;
-  temp2:any;
-  authorName:string=""
+  temp2: any;
+  authorName: string = '';
 
   constructor(private _auth: AuthGardGuard, private _userBooks: DataService) {
     this.temp = this._userBooks.getData('/userBooks/all').subscribe((res) => {
       this.data = res;
       this.shownData = this.data[0].books;
-      
-      console.log(this.data[0].books)
-      
+
+      console.log(this.data[0].books);
     });
   }
 
   changeShelf(choosenShelf: string) {
-    this.shelf=choosenShelf
-   
-    if(this.shelf!="ALL"){
-      
-      this.shownData = this.data[0].books.filter((item:any)=>{
-     return item.shelf==this.shelf  
-      })
-         }else{
-          this.shownData = this.data[0].books
-         }
+    this.shelf = choosenShelf;
 
-       
-        
-        }
-        
-  
-
-  onSelected(item:any,shelf:string){
-   
-      this._userBooks.patchData('/userBooks/shelf',{"bookId":item.bookId._id,
-      "shelf":shelf}).subscribe((res)=>{
-        console.log(res)
-      })
-    
+    if (this.shelf != 'ALL') {
+      this.shownData = this.data[0].books.filter((item: any) => {
+        return item.shelf == this.shelf;
+      });
+    } else {
+      this.shownData = this.data[0].books;
     }
+  }
 
-    getAuthorName(id:string){
-     this.temp2= this._userBooks.getData(`/author/${id}`).subscribe((res:any)=>{
-      
-        this.authorName = `${res.firstName} ${res.lastName}`
-     })
-     
-      return this.authorName
-      
-    }
+  onSelected(item: any, shelf: string) {
+    this._userBooks
+      .patchData('/userBooks/shelf', { bookId: item.bookId._id, shelf: shelf })
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 
+  getAuthorName(id: string) {
+    this.temp2 = this._userBooks
+      .getData(`/author/${id}`)
+      .subscribe((res: any) => {
+        this.authorName = `${res.firstName} ${res.lastName}`;
+      });
 
+    return this.authorName;
+  }
 
   UpdateRating(item: any, value: number) {
     item.rating = value;
-    this._userBooks.patchData('/userBooks/rating',{"bookId":item.bookId._id,
-    "rating":value}).subscribe((res)=>{
-      console.log(res)
-    }) 
+    this._userBooks
+      .patchData('/userBooks/rating', {
+        bookId: item.bookId._id,
+        rating: value,
+      })
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
 
-  ngOnDestroy(): void {
-    this.temp2.unsubscribe();
-  }
+  ngOnDestroy(): void {}
 }
